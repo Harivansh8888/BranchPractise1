2ef694a65f3653c6f8e3e846cb14b08fd8d91274
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-03-07 10:15:57 +0530

    Inserted Member Function City

--- practise.cpp
+++ practise.cpp
@@ -10,11 +10,20 @@ class simple
 			cin>>name;
 			cout<<"Your Name is: "<<name<<endl;
 		}
+		void City()
+		{
+			string city;
+			cout<<"Enter the City you belong: "<<endl;
+			cin>>city;
+			cout<<"Your City is: "<<city<<endl;
+		}
+
 };
 int main()
 {
 	simple s1;
 	s1.Name();
+	s1.City();
 	return 0;
 }
 
