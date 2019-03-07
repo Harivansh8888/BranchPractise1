14b533905cf84c6c44f634c4754904dd8b0b15f9
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-03-07 10:20:22 +0530

    Inserted Member Function Age

--- practise.cpp
+++ practise.cpp
@@ -10,11 +10,19 @@ class simple
 			cin>>name;
 			cout<<"Your Name is: "<<name<<endl;
 		}
+		void Age()
+		{
+			int age;
+			cout<<"Enter your Age: "<<endl;
+			cin>>age;
+			cout<<"Your Age is: "<<age<<endl;
+		}
 };
 int main()
 {
 	simple s1;
 	s1.Name();
+	s1.Age();
 	return 0;
 }
 
